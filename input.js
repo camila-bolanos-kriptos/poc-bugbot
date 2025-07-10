8df6087d7fd30eaa3b49fe7fa19e9de59197d96a
Camila Bolaños <cbolanos_intern@kriptos.io>
2025-07-10 12:29:47 -0500

    test: funcionalidad bugbot,cursor review

--- input.js
+++ input.js
@@ -19,7 +19,3 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(`<h1>Hola ${userInput}</h1>`);  // No sanitizado
 });
-
-server.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
